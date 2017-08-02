25b9a0eab1f4c7ad8c5e946832a9c6a97c2a0779
smaguire <maguire.sean037@gmail.com>
2017-08-01 18:13:16 -0700

    SpaceBar
    Got the Space Bar Operational

---
+++ Assets/Scripts/TextPro.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextPro : MonoBehaviour {
+
+    public TextMeshProUGUI tutorial;
+
+	// Use this for initialization
+	void Start () {
+        tutorial = GameObject.Find("Tutorial").GetComponent<TextMeshProUGUI>();
+        tutorial.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
