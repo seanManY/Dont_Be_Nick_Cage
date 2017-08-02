16b6fe6d4baefd33f50cba40bc78c2616ce741d2
smaguire <maguire.sean037@gmail.com>
2017-08-02 09:04:14 -0700

    Space
    What a wonder

--- Assets/Scripts/TextPro.cs
+++ Assets/Scripts/TextPro.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class TextPro : MonoBehaviour {
 
     public TextMeshProUGUI tutorial;
+    public float counter = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,17 @@ public class TextPro : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        //tutorial
+        counter -= Time.deltaTime;
+        if(counter < 0)
+        {
+            tutorial.gameObject.SetActive(true);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            tutorial.gameObject.SetActive(false);
+        }        
 	}
 }
