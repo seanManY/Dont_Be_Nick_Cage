bcf83642bef19110a610b0ca1586621467f418f1
smaguire <maguire.sean037@gmail.com>
2017-08-01 17:11:41 -0700

    Changes?

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour {
+
+    public Button play;
+    public Button Instructions;
+    public Button Exit;
+
+    public Image Arrow;
+
+	// Use this for initialization
+	void Start () {
+
+        play = GameObject.Find("Play").GetComponent<Button>();
+        play.onClick.AddListener(playEvent);
+
+        Instructions = GameObject.Find("Intructions").GetComponent<Button>();
+        Instructions.onClick.AddListener(instructionEvent);
+
+        Exit = GameObject.Find("Exit").GetComponent<Button>();
+        Exit.onClick.AddListener(exitEvent);
+
+        Arrow = GameObject.Find("Arrow").GetComponent<Image>();
+        Arrow.gameObject.SetActive(false);
+
+    }
+
+    void playEvent()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    void instructionEvent()
+    {
+        Arrow.gameObject.SetActive(true);
+    }
+
+   void exitEvent()
+    {
+        Application.Quit();
+    }
+}
