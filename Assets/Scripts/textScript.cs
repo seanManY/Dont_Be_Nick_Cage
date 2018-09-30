997e52c99a95f3c3df01ee28d564c813cca3c843
Sean Maguire <maguire.sean037@gmail.com>
2018-09-30 16:40:23 -0700

    new new

---
+++ Assets/Scripts/textScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textScript : MonoBehaviour {
+
+    public TextMeshPro text;
+
+    void Awake()
+    {
+        text = GetComponent<TextMeshPro>();
+        text.faceColor = new Color32(255, 128, 0, 255);
+    }
+}
